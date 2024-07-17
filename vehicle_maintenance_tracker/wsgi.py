89dfd3d57354e9c62d7a87193ddf3cd7cbb97c56
Dylan Moreland <dylan.c.moreland@gmail.com>
2024-07-16 18:05:36 -0700

    Change project names & associated variables to vehicle_maintenance_tracker

--- vehicle_maintenance_tracker/wsgi.py
+++ vehicle_maintenance_tracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for personal_crm project.
+WSGI config for vehicle_maintenance_tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_crm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_maintenance_tracker.settings')
 
 application = get_wsgi_application()

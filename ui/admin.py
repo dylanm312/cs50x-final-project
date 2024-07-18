2f1354a3db5752cf91442fc9a802f1f528aacf54
Dylan Moreland <dylan.c.moreland@gmail.com>
2024-07-17 23:48:23 -0700

    Register models in admin interface

--- ui/admin.py
+++ ui/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Vehicle, MaintenanceItem, VehicleEvent
+
 # Register your models here.
+admin.site.register(Vehicle)
+admin.site.register(MaintenanceItem)
+admin.site.register(VehicleEvent)

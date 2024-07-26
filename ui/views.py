384afa546bf8bf6b1ac45fdd8e659cb2ab0f86ac
Dylan Moreland <dylan.c.moreland@gmail.com>
2024-07-26 10:19:53 -0700

    Add vehicle list and vehicle edit page

--- ui/views.py
+++ ui/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404
+from django.views import generic
+
+from .models import Vehicle, VehicleEvent
 
 # Create your views here.
-def index(request):
-    return render(request, "ui/index.html")
+class IndexView(generic.ListView):
+    template_name = "ui/index.html"
+    context_object_name = "vehicle_list"
+    
+    def get_queryset(self) -> QuerySet[Vehicle]:
+        return Vehicle.objects.all()
+    
+class VehicleDetailView(generic.DetailView):
+    template_name = "ui/edit_vehicle.html"
+    context_object_name = "vehicle"
+    
+    def get_object(self) -> Vehicle:
+        return get_object_or_404(Vehicle, pk=self.kwargs["pk"])
+        # return VehicleEvent.objects.filter(vehicle_id=vehicle.id).order_by("timestamp")
+        

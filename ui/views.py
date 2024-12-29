3ca8aeaabf605c14d28b9f5f9db68190b710b29d
Dylan Moreland <dylan.c.moreland@gmail.com>
2024-12-29 14:01:22 -0800

    Get everything working

--- ui/views.py
+++ ui/views.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from django.db.models.query import QuerySet
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.contrib.messages.views import SuccessMessageMixin
+
+from vehicle_maintenance_tracker import settings
 
 from .models import MaintenanceItem, Vehicle, VehicleEvent
 
@@ -42,52 +45,42 @@ class VehicleDeleteView(SuccessMessageMixin, generic.DeleteView):
     success_url = reverse_lazy("index")
     success_message = "Deleted vehicle '%(name)s'"
     
+    
+    
+    
 class VehicleEventCreateView(SuccessMessageMixin, generic.edit.CreateView):
     model = VehicleEvent
     fields = ["name", "description", "timestamp", "mileage", "maintenance_item"]
     template_name_suffix = "_form"
-    success_url = reverse_lazy("index")
-    success_message = "Added maintenance event for %(vehicle)s at %(timestamp)s with %(maintenance_item)s."
+    success_message = "Added maintenance event %(name)s."
+    
+    def get_success_url(self):
+        return reverse("view_vehicle", kwargs={"pk": self.object.vehicle.pk})
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         maintenance_options = MaintenanceItem.objects.all()
-        context.update({"maintenance_options": maintenance_options})
+        context.update({
+            "maintenance_options": maintenance_options,
+            "vehicle": Vehicle.objects.get(pk=self.kwargs["pk"]),
+            "now": datetime.now(),
+        })
         return context
-    # def get(self, request, *args, **kwargs):
-    #     maintenance_options = MaintenanceItem.objects.all()
-    #     context = {
-    #         "vehicle": Vehicle.objects.get(pk=kwargs["pk"]),
-    #         "maintenance_options": maintenance_options
-    #     }
-    #     return render(request, "ui/maintenance_event_form.html", context)
     
-    # def post(self, request, *args, **kwargs):
-    #     name = request.POST["name"]
-    #     description = request.POST["description"]
-    #     vehicle = Vehicle.objects.get(pk=kwargs["pk"])
-    #     maintenance_item = MaintenanceItem.objects.get(pk=request.POST["maintenance_item"])
-    #     timestamp = request.POST["timestamp"]
-    #     mileage = round(int(request.POST["mileage"]), 1)
-    #     event = VehicleEvent(
-    #         name=name,
-    #         description=description,
-    #         vehicle=vehicle,
-    #         maintenance_item=maintenance_item,
-    #         timestamp=timestamp,
-    #         mileage=mileage)
-    #     event.save()
-        
-    #     messages.success(request, f"Added maintenance event for {vehicle} at {timestamp} with {maintenance_item}.")
-        
-    #     return redirect("index")
+    # Add foreign key relationship upon submission
+    def form_valid(self, form):
+        form.instance.vehicle = Vehicle.objects.get(pk=self.kwargs["pk"])
+        form.save()
+        return super().form_valid(form)
     
 class VehicleEventUpdateView(SuccessMessageMixin, generic.edit.UpdateView):
     model = VehicleEvent
     fields = ["name", "description", "timestamp", "mileage", "maintenance_item"]
     template_name_suffix = "_form"
-    success_url = reverse_lazy("index")
     success_message = "Updated maintenance event '%(name)s'"
+    
+    def get_success_url(self):
+        return reverse("view_vehicle", kwargs={"pk": self.object.vehicle.pk})
     
     def get_object(self):
         return VehicleEvent.objects.get(pk=self.kwargs["event_pk"])
@@ -95,13 +88,25 @@ class VehicleEventUpdateView(SuccessMessageMixin, generic.edit.UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         maintenance_options = MaintenanceItem.objects.all()
-        context.update({"maintenance_options": maintenance_options})
-        
-        # Mark selected maintenance option if any
-        context.update({"selected_maintenance_item": self.object.maintenance_item})
+        context.update({
+            "maintenance_options": maintenance_options,
+            "selected_maintenance_item": self.object.maintenance_item,
+            "vehicle": self.object.vehicle,
+        })
         return context
+    
+    # Add foreign key relationship upon submission
+    def form_valid(self, form):
+        form.instance.vehicle = self.object.vehicle
+        form.save()
+        return super().form_valid(form) 
     
 class VehicleEventDeleteView(SuccessMessageMixin, generic.DeleteView):
     model = VehicleEvent
-    success_url = reverse_lazy("index")
-    success_message = "Deleted maintenance event '%(name)s'"
+    success_message = "Deleted maintenance event"
+    
+    def get_success_url(self):
+        return reverse("view_vehicle", kwargs={"pk": self.object.vehicle.pk})
+    
+    def get_object(self):
+        return VehicleEvent.objects.get(pk=self.kwargs["event_pk"])
